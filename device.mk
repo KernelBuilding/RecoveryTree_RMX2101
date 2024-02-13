7ab65eb1942739bb52d1989eb4c7d81fc28933bd
ProjectKernel <155278452+KernelBuilding@users.noreply.github.com>
2024-02-14 04:15:23 +0700

    Update device.mk

--- device.mk
+++ device.mk
@@ -19,7 +19,6 @@ PRODUCT_SHIPPING_API_LEVEL := 29
 # Dynamic partition stuff
 PRODUCT_USE_DYNAMIC_PARTITIONS := true
 
-
 # Fastbootd stuff
 PRODUCT_PACKAGES += \
     android.hardware.fastboot@1.0-impl-mock
